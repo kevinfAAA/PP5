d7e00300bebaa2e57ae8a23557ca911202810778
kevinfAAA <kevin.foley21@hotmail.com>
2022-02-19 20:27:00 +0000

    Added video app and CRUD functions

--- kevins_pp5/settings.py
+++ kevins_pp5/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'crispy_forms',
     'home',
+    'videos',
 ]
 
 MIDDLEWARE = [
@@ -154,7 +155,10 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- kevins_pp5/urls.py
+++ kevins_pp5/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from videos import views as video_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('videos/', video_views.index, name='index'),
+    path('videos/', include('videos.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
