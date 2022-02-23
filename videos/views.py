57ea0ecf28d6d4125d3503be5677bdeb1a212cf5
kevinfAAA <kevin.foley21@hotmail.com>
2022-02-23 20:19:41 +0000

    Add category and comment models, and comment form

--- videos/views.py
+++ videos/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, reverse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from .models import Video
+from django.views import View
+from .models import Video, Comment, Category
+from .forms import CommentForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
@@ -14,7 +16,7 @@ class Index(ListView):
 
 class CreateVideo(LoginRequiredMixin, CreateView):
     model = Video
-    fields = ['title', 'description', 'video_file', 'thumbnail']
+    fields = ['title', 'description', 'video_file', 'thumbnail', 'category']
     template_name = 'videos/create_video.html'
 
     def form_valid(self, form):
@@ -25,9 +27,46 @@ class CreateVideo(LoginRequiredMixin, CreateView):
         return reverse('video-detail', kwargs={'pk': self.object.pk})
 
 
-class DetailVideo(LoginRequiredMixin, DetailView):
-    model = Video
-    template_name = 'videos/detail_video.html'
+class DetailVideo(View):
+    def get(self, request, pk, *args, **kwargs):
+        video = Video.objects.get(pk=pk)
+
+        form = CommentForm()
+        comments = Comment.objects.filter(video=video).order_by('-created_on')
+        categories = Video.objects.filter(category=video.category)[:15]
+
+
+        context = {
+            'object': video,
+            'comments': comments,
+            'categories': categories,
+            'form': form
+        }
+        return render(request, 'videos/detail_video.html', context)
+
+    def post(self, request, pk, *args, **kwargs):
+        video = Video.objects.get(pk=pk)
+
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                user=self.request.user,
+                comment=form.cleaned_data['comment'],
+                video=video
+            )
+            comment.save()
+
+        comments = Comment.objects.filter(video=video).order_by('-created_on')
+        categories = Video.objects.filter(category=video.category)[:15]
+
+
+        context = {
+            'object': video,
+            'comments': comments,
+            'categories': categories,
+            'form': form
+        }
+        return render(request, 'videos/detail_video.html', context)
 
 
 class UpdateVideo(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
@@ -53,3 +92,15 @@ class DeleteVideo(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         video = self.get_object()
         return self.request.user == video.uploader
+
+
+class VideoCategoryList(View):
+    def get(self, request, pk, *args, **kwargs):
+        category = Category.objects.get(pk=pk)
+        videos = Video.objects.filter(category=pk).order_by('-date_posted')
+        context = {
+            'category': category,
+            'videos': videos
+        }
+
+        return render(request, 'videos/video_category.html', context)       
