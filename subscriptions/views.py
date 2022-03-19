dc26914bd740705c4fb17561d6e9346c46351fbb
kevinfAAA <kevin.foley21@hotmail.com>
2022-03-19 11:50:40 +0000

    Final push before heroku deployment

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,36 +1,51 @@
 import stripe
 from django.conf import settings
-from django.contrib.auth.decorators import login_required
-from django.http.response import JsonResponse
+from django.contrib.auth.models import User
+from django.http.response import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
+from subscriptions.models import StripeCustomer
 
 
-# @login_required
 def home(request):
-    return render(request, 'subscriptions/home.html')
+    try:
+        # Retrieve the subscription & product
+        stripe_customer = StripeCustomer.objects.get(user=request.user)
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        subscription = stripe.Subscription.retrieve(stripe_customer.stripeSubscriptionId)
+        product = stripe.Product.retrieve(subscription.plan.product)
+
+        # Feel free to fetch any additional data from 'subscription' or 'product'
+        # https://stripe.com/docs/api/subscriptions/object
+        # https://stripe.com/docs/api/products/object
+
+        return render(request, 'subscriptions/home.html', {
+            'subscription': subscription,
+            'product': product,
+        })
+
+    except StripeCustomer.DoesNotExist:
+        return render(request, 'subscriptions/home.html')
 
 
-# @login_required
 def success(request):
     return render(request, 'subscriptions/success.html')
 
 
-# @login_required
 def cancel(request):
     return render(request, 'subscriptions/cancel.html')
 
 
-
-# @csrf_exempt
+@csrf_exempt
 def stripe_config(request):
     if request.method == 'GET':
         stripe_config = {'publicKey': settings.STRIPE_PUBLISHABLE_KEY}
         return JsonResponse(stripe_config, safe=False)
 
 
-# @csrf_exempt
+# Checkout Session
+@csrf_exempt
 def create_checkout_session(request):
     if request.method == 'GET':
         domain_url = 'https://8000-kevinfaaa-pp5-1sz0o8xrchw.ws-eu34.gitpod.io/subscriptions/'
@@ -52,3 +67,44 @@ def create_checkout_session(request):
             return JsonResponse({'sessionId': checkout_session['id']})
         except Exception as e:
             return JsonResponse({'error': str(e)})
+
+
+# Stripe Webhook
+@csrf_exempt
+def stripe_webhook(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    endpoint_secret = settings.STRIPE_ENDPOINT_SECRET
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+
+        # Fetch all the required data from session
+        client_reference_id = session.get('client_reference_id')
+        stripe_customer_id = session.get('customer')
+        stripe_subscription_id = session.get('subscription')
+
+        # Get the user and create a new StripeCustomer
+        user = User.objects.get(id=client_reference_id)
+        StripeCustomer.objects.create(
+            user=user,
+            stripeCustomerId=stripe_customer_id,
+            stripeSubscriptionId=stripe_subscription_id,
+        )
+        print(user.username + ' just subscribed.')
+
+    return HttpResponse(status=200)
